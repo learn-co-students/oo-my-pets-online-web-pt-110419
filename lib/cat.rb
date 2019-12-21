6821da487b659d73f1b387697021b96a08338b33
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 17:38:01 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :breed
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  jack = Cat.new("Jack")
+  jack.name
+end
+
+  savannah = Cat.breed("Savannah")
+  savannah.breed 
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,45 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets 
+ 
+  def initialize(Owner)
+    @owner = owner
+  end
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  bill = Owner.new("Bill")
+  bill.owner
 end
+  bill = Name.new("Bill")
+  bill.name
+end
+
+bill.owner.name
+end
+
+class Owner
+  attr_accessor :name, :pets
+ 
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+ 
+  def add_pets(pets)
+    @pets << pets
+    owner.pets = self
+  end
+ 
+  def pets
+    @pets
+  end
+end
+
+bill.add_pets(bane)
+bill.add_pets(jack)
+
+bane.owner.name
+jack.owner.name
+
