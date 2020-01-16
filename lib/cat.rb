8e5e43a71f330aa785d14dfc0cea765d7e5f9430
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-16 00:22:18 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,28 @@
 class Cat
-attr_accessor :name 
+# attr_accessor :name 
 
-def initialize 
-  @name = name 
-end 
+# def initialize 
+#   @name = name 
+# end 
 
+# end
+# 
+ attr_accessor :owner, :mood
+  attr_reader :name 
+  @@all = []
+ 
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end 
+    
+    def self.all
+      @@all
+    end 
+  
 end
+
+
